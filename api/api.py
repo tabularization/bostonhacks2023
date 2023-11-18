08c2ae40f22171d7d62804f2f79dc4804e415f3a
Eric Lin <93724436+Rinciel@users.noreply.github.com>
2023-11-18 14:13:35 -0500

    Add api

--- api/api.py
+++ api/api.py
@@ -2,13 +2,69 @@
 
 import os
 from dotenv import load_dotenv
+from openai import OpenAI
+import summary
 
-OPENAI_KEY = None
+client = None
+
+prompt = """
+Continue the story based on context and current user input. 
+Do not deviate from the story. After each interaction, 
+add event that advances said story by guiding the user. Max length 150"""
+story = "You wake up in a post-apocalyptic world"
+
 def init():
     """Initialize the API."""
     # load .env file
     load_dotenv()
     # set openai key
-    global OPENAI_KEY
-    OPENAI_KEY = os.getenv("OPENAI_KEY")
+    global client
+    client = OpenAI(
+        api_key=os.getenv("OPENAI_KEY"),
+    )
+    summary.init()
+
+# ===============================================================================
+context = []
+def get_context():
+    # lets the ai remember the last few interactions
+    global context
+    if len(context) > 5:
+        context = context[-5:]
+    return context
+
+def add_context(text):
+    global context
+    context.append(text)
+    # remove summary if it gets too long
+    if len(summary) > 5:
+        summary = summary[-5:]
+# ===============================================================================
+# make api calls
+def get_response(text):
+    """Get a response from the API."""
+    # create a context
+
+    response = client.chat.completions.create(
+        messages=[
+            {
+                "role": "system",
+                "content": prompt + "\n\n" + story + "\n\n" + "\n\n".join(context) + "\n\n" + "User: " + text
+            },
+        ],
+        model="gpt-3.5-turbo-1106",
+        max_tokens=150
+    )
+    # get content from response
+    content = response.choices[0].message.content
+    print(content)
+    # add to summary
+    context.append(summary.summarize(content, 1))
     
+
+# test
+if __name__ == "__main__":
+    init()
+    while True:
+        text = input("You: ")
+        get_response(text)
---
+++ api/summary.py
@@ -0,0 +1,43 @@
+import spacy
+from spacy.lang.en.stop_words import STOP_WORDS
+from string import punctuation
+from collections import Counter
+from heapq import nlargest
+
+# https://medium.com/analytics-vidhya/text-summarization-using-spacy-ca4867c6b744
+
+nlp = None
+def init():
+    global nlp
+    nlp = spacy.load('en_core_web_sm')
+
+def summarize(text, n):
+    # print current time
+    global nlp
+    doc = nlp(text)
+    keyword = []
+    stopwords = list(STOP_WORDS)
+    pos_tag = ['PROPN', 'ADJ', 'NOUN', 'VERB']
+    for token in doc:
+        if(token.text in stopwords or token.text in punctuation):
+            continue
+        if(token.pos_ in pos_tag):
+            keyword.append(token.text)
+    freq_word = Counter(keyword)
+    max_freq = Counter(keyword).most_common(5)
+
+    max_freq = Counter(keyword).most_common(1)[0][1]
+    for word in freq_word.keys():
+        freq_word[word] = (freq_word[word]/max_freq)
+    freq_word.most_common(5)
+
+    sent_strength={}
+    for sent in doc.sents:
+        for word in sent:
+            if word.text in freq_word.keys():
+                if sent in sent_strength.keys():
+                    sent_strength[sent]+=freq_word[word.text]
+                else:
+                    sent_strength[sent]=freq_word[word.text]
+    summary = nlargest(n, sent_strength, key=sent_strength.get)
+    return " ".join([word.text for word in summary])
