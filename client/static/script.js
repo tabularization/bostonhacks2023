dcb64d757643e860b8510c36006a6ec7462c5a74
tabularization <dennis@crc-dot1x-nat-10-239-95-150.bu.edu>
2023-11-18 16:02:51 -0500

    api test

--- client/static/script.js
+++ client/static/script.js
@@ -1,1 +1,21 @@
-some scriptp lol
+document.querySelector('.search-box a').addEventListener('click', function(e) {
+    e.preventDefault();
+    var inputValue = document.querySelector('.search-box input').value;
+    
+    var data = { search: inputValue }; // This is the data we want to send in JSON format
+
+    fetch('/api', {
+        method: 'POST', // or 'PUT'
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log('Success:', data);
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+    });
+});
