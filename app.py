89f19a0ba06098a7779ae49913b6fff43daf22c3
Eric Lin <93724436+Rinciel@users.noreply.github.com>
2023-11-19 01:29:10 -0500

    remove __main__

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ app = Flask(__name__)
 app.register_blueprint(client_bp)
 app.register_blueprint(api_bp)
 
-if __name__ == "__main__":
-    api.api.init()
-    api.summary.init()
-    app.run()
+api.api.init()
+api.summary.init()
+app.run()
